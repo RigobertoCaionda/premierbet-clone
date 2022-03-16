d2c2f8d664fee233a3ca4756f6851c0b6cd57cef
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2022-03-16 02:09:19 +0100

    Adding things

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { Routes, Route, useRoutes } from "react-router-dom";
+import Home from "./pages/Home";
+import Teste from "./pages/Teste";
 const Page = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
-    )
-}
+  return useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/teste", element: <Teste /> },
+  ]);
+};
 export default Page;
---
+++ src/pages/Teste/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "../../App.css";
+
+SwiperCore.use([EffectCoverflow, Pagination]);
+const slide_img = [
+  "/assets/monaco.svg",
+  "/assets/benfica.svg",
+  "/assets/monaco.svg",
+  "/assets/benfica.svg",
+];
+const Page = () => {
+  return (
+    <div>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={2}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        pagination={true}
+        className="mySwiper"
+      >
+        {slide_img.map((img, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <img src={img} alt="" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+export default Page;
