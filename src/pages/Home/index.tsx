850107c7a4bf7b1913ab098aff7cef40af9b0e84
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2022-03-17 17:47:18 +0100

    Creating the slider

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,52 @@
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
 import * as C from "./styled";
+
+SwiperCore.use([EffectCoverflow, Pagination]);
+const slide_img = [
+  "/assets/slide1.jpg",
+  "/assets/slide2.jpg",
+  "/assets/slide3.jpg",
+  "/assets/slide4.jpg",
+  "/assets/slide5.jpg",
+];
 const Page = () => {
   return (
     <C.HomeArea>
-      <C.SliderArea>Slider</C.SliderArea>
+      <C.SliderArea>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={false}
+          centeredSlides={true}
+          slidesPerView={1}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          pagination={true}
+          className="mySwiper"
+        >
+          {slide_img.map((img, i) => {
+            return (
+              <SwiperSlide key={i}>
+                <img src={img} alt="" />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </C.SliderArea>
+
+      <C.MainArea>
+        <C.LeftSideContent>LeftSide</C.LeftSideContent>
+        <C.MainContent>MainSide</C.MainContent>
+        <C.RightSideContent>RightSide</C.RightSideContent>
+      </C.MainArea>
     </C.HomeArea>
   );
 };
--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -4,4 +4,22 @@ export const HomeArea = styled.div`
 
 `;
 
-export const SliderArea = styled.div``;
+export const SliderArea = styled.div`
+    background-color: #dddddd;
+`;
+export const MainArea = styled.div`
+    display: flex;
+    background-color: #dddddd;
+    padding: 3px;
+`;
+
+export const LeftSideContent = styled.aside`
+    background-color: red;
+`;
+export const RightSideContent = styled.aside`
+background-color: blue;
+`;
+export const MainContent = styled.main`
+    flex: 1;
+    background-color: yellow;
+`;
